e4b6412281ae0f9cd71158ab4bdfd6cc5e2994a2
brian1224 <kenny19861224@gmail.com>
2019-01-26 14:45:33 +0800

    tre

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ex
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tiger
 # image name
 #IMAGE=iii-devops-ecr
-IMAGE=brian
+IMAGE=tiger
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"

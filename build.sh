a4279edd5cc582f698af5450bce6b181f3539dbc
brian1224 <kenny19861224@gmail.com>
2019-01-26 17:32:51 +0800

    five

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -ex
 #set registry
 #USERNAME=129729052534.dkr.ecr.ap-northeast-1.amazonaws.com
-USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
+USERNAME=fuck
 # image name
 #IMAGE=iii-devops-ecr
 IMAGE=tiger
